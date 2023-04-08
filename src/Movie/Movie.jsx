2ad920e46833d43f720d9cdec170d322dc968f06
ZuzanaMatiaskova <zuzkamatiaskova@gmail.com>
2023-04-08 09:27:52 +0200

    Hotovo

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -14,12 +14,27 @@ const Movie = () => {
             <img src={movieData.poster} alt={movieData.title} />
 
             <h2>{movieData.title}</h2>
+            <p><b>Rok vydání: </b>{movieData.year}</p>
+            <p><b>Hodnocení: </b>{movieData.rating}</p>
+            <p><b>Režisér: </b>{movieData.director}</p>
+            <p><b>Žánr: </b>{movieData.genre}</p>
 
-            <p>{movieData.storyline}</p>
+            {movieData.cast.map((cast) => (
+                <table key={cast.name}>
+                    <tbody>
+                        <tr>
+                            <td><b>Jméno herce: </b>{cast.name}</td>
+                            <td><b>Role: </b>{cast.as}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            ))
+            }
+
+            <p><b>Popis: </b>
+                {movieData.storyline}</p>
 
         </div>
-
-
     )
 }
 export default Movie
